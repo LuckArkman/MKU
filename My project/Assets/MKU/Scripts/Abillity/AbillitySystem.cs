3272acfdd005c21265db1049b31f279057da0a78
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-04-11 02:20:40 -0300

    up

---
+++ My project/Assets/MKU/Scripts/Abillity/AbillitySystem.cs
@@ -0,0 +1,39 @@
+﻿using MKU.Scripts.Enums;
+using MKU.Scripts.Singletons;
+using UnityEngine;
+using CharacterController = MKU.Scripts.Character.CharacterController;
+
+namespace MKU.Scripts.Abillity
+{
+    public class AbillitySystem : StateMachineBehaviour
+    {
+        public states state = states.None;
+        public AbillityController abillity = null;
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            abillity.OnStateEnter(animator, stateInfo, layerIndex);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            abillity.OnStateExit(animator, stateInfo, layerIndex);
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (Singleton.Instance.controller.isMoving)animator.SetBool(states.Walk.ToString(), Singleton.Instance.controller.isMoving);
+            if (Singleton.Instance.controller.isRunning)animator.SetBool(states.Run.ToString(), Singleton.Instance.controller.isRunning);
+            abillity.OnStateUpdate(animator, stateInfo, layerIndex);
+        }
+
+        public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            abillity.OnStateMove(animator, stateInfo, layerIndex);
+        }
+
+        public override void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            abillity.OnStateIK(animator, stateInfo, layerIndex);
+        }
+    }
+}
---
+++ My project/Assets/MKU/Scripts/Singletons/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using CharacterController = MKU.Scripts.Character.CharacterController;
+
+namespace MKU.Scripts.Singletons
+{
+    public class Singleton : MonoBehaviour
+    {
+        public CharacterController controller = null;
+
+        private static Singleton instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    // Tenta encontrar na cena
+                    instance = FindObjectOfType<Singleton>();
+
+                    // Se não existir, cria automaticamente
+                    if (instance == null)
+                    {
+                        GameObject obj = new GameObject("Singleton");
+                        instance = obj.AddComponent<Singleton>();
+
+                        Debug.Log("[Singleton] Instância criada automaticamente.");
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
